8c9022dc9f32455f7c473909e8e3ff5a05d5e913
unknown <jshyun912@naver.com>
2026-05-01 17:29:58 +0900

    init

---
+++ src/main/java/io/thelogmaster/blogmaster/BlogMasterApplication.java
@@ -0,0 +1,13 @@
+package io.thelogmaster.blogmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlogMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/thelogmaster/blogmaster/BlogMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package io.thelogmaster.blogmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
